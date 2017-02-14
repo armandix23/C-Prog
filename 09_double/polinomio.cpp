186f413d2951391d9684d69478345b9051865103
Armandix23 <armando@gmail.com>
2017-02-15 00:49:51 +0100

    Polinomios

---
+++ 09_double/polinomio.cpp
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  polinomio.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  15/02/17 00:23:31
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+int main(int argc, char *argv[]){
+
+      int coeficiente[N];
+      int x = 2;
+      int potencia = 1;
+      int max_cof;
+      int resultado = 0;
+
+      printf("Indicar numero de coeficientes: \n");
+      scanf(" %i", &max_cof);
+
+      printf("Introducir los coeficientes: \n");
+
+      for(int i=0; i <= max_cof; i++){
+      scanf(" %d", &coeficiente[i]);
+      }
+
+      for(int j=0; j <= max_cof; j++,potencia*=x)
+      resultado+=coeficiente[j]*potencia;
+
+      printf("Resultado es: %i \n", resultado);
+
+      return EXIT_SUCCESS;
+}
